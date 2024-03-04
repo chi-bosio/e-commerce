1cfda340569c91d2bec6cd9862e94f6395fc8276
chiarabosio2023 <chiaravalentinabosio@gmail.com>
2024-03-04 16:38:45 -0300

    configurando app y routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
 const express=require('express');
-const productRouter = require('./routes/productRouter.js');
-const cartRouter = require('./routes/cartRouter.js')
+const http = require('http')
+const {Server}= require("socket.io")
+const engine = require('express-handlebars').engine
 
-const PORT = 8080;
-const app = express();
+const productRouter = require('./routes/productRouter.js')
+const cartRouter = require('./routes/cartRouter.js')
+const handlebarsRouter = require('./routes/handlebarsRouter.js')
+const socketioRouter = require('./routes/socketIO.js')
+
+const PORT = 8080
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
-
+app.use('/', handlebarsRouter)
+app.use('/socketio', socketioRouter)
 
 app.get('/', (req, res) => {
     res.send('Bienvenido a mi localhost...!!!')
 })
+
 app.get('*', (req, res) => {
     res.send('Error 404 - Not Found')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server Online en puerto ${PORT}`);
 });
