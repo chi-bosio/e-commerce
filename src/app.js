5f0712d58ea8494f466e85e64a8073800a10df22
chiarabosio2023 <chiaravalentinabosio@gmail.com>
2024-04-29 17:31:00 -0300

    arreglando app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express=require('express');
-const http = require('http')
 const socketIO = require('socket.io')
 const engine = require('express-handlebars').engine
 const path = require('path')
 const mongoose = require('mongoose')
-const session = require('express-session')
 const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
 
 const passportConfig = require('./config/passportConfig.js')
 
@@ -16,22 +16,17 @@ const sessionRouter = require('./routes/sessionsRouter.js')
 
 const viewRouter = router
 
-const PORT = 8080
+const PORT = process.env.COOKIE_SECRET || 8080
 const app = express()
-const server = http.createServer(app)
+
 const io = socketIO(server)
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true
-}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 passportConfig()
 app.use(passport.initialize())
-app.use(passport.session())
 
 app.engine('handlebars', engine({
     runtimeOptions: {
@@ -49,6 +44,7 @@ app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 app.use('/api/sessions', sessionRouter)
 
+
 app.get('*', (req, res) => {
     res.send('Error 404 - Not Found')
 })
@@ -61,8 +57,8 @@ handleRealTimeProductsSocket(io)
 
 const connect = async () => {
     try{
-        await mongoose.connect("mongodb+srv://chibosio:Mika&Silver@cluster0.3jin0k1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=ecommerce")
-        console.log("DB online...!!");
+        await mongoose.connect(`mongodb+srv://chibosio:${process.env.MONGO_PASSWORD}@cluster0.3jin0k1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=ecommerce`)
+        console.log("DB online...!!");z
     } catch(error){
         console.log("Conexión fallida. Detalle:", error.message);
     }
